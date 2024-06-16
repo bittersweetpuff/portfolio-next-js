72285d46f8c8d01709894933c72caf60b9475499
bittersweetpuff <biszkoptex@gmail.com>
2024-06-16 14:37:54 +0200

    Responsibility fixes + data changes

--- components/project.tsx
+++ components/project.tsx
@@ -25,9 +25,10 @@ export default function Project({ title, description, tags, imageUrl}: ProjectPr
           opacity: opacityProgress,
         }}
         className="mb-3 sm:mb-8 last:mb-0 group group-even:pl-8">
-      <section className = "bg-gray-100 max-w-[42rem] rounded-lg border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition dark:bg-white/10 dark:hover:bg-white/20">
+      <section className = "bg-gray-100 max-w-[42rem] rounded-lg border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:bg-white/10 dark:hover:bg-white/20">
       
-      <div className="py-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
+      <div className="py-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 
+      sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
       <h3 className="text-2xl font-semibold">{title}</h3>
       <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">{description}</p>
       <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
--- lib/data.ts
+++ lib/data.ts
@@ -64,24 +64,24 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: "Characters",
+    title: "Companions",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+      "I worked with a band of greatest adventurers and warriors this land has ever seen",
+    tags: ["Fighters", "Rogues", "Wizards", "Druids", "Paladins"],
     imageUrl: charactersImg,
   },
   {
-    title: "UI Design",
+    title: "Trophies",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+      "In my life as a dragon slayer I managed to kill and collect trophies form many vile creatures",
+    tags: ["Dragons", "Wyverns", "Trolls", "Drakes", "Wyrms"],
     imageUrl: uidesignImg,
   },
   {
-    title: "Illustrations",
+    title: "Weapons",
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+      "My inventory and stash is full of enchanted weapons, ready to be used in fight agains evil",
+    tags: ["Swords", "Axes", "Spears", "Bows"],
     imageUrl: illustrationsImg,
   },
 ] as const;
